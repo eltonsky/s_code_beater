44bb1ee9d1ed3f396496a7c8339d674551349500
eltonsky <eltonsky9404@gmail.com>
2013-07-07 14:14:38 +1000

    2nd case

--- s_code_beater.py
+++ s_code_beater.py
@@ -11,6 +11,9 @@ class Line:
     indent=0
     if_print=1
 
+    #static
+    commContinue = 0
+
     def __init__(self, line, indent=0, if_print=1):
         self.val = line
         self.indent = indent
@@ -24,49 +27,73 @@ class Line:
         
         sys.stdout.write(self.val)
 
+    @staticmethod
     def add_indent(count):
         inds = ""
-        for i in range(0,count):
+        for i in range(1,count+1):
             inds += '\t'
 
         return inds
 
     def process(self):
-        eff_indent = self.indent
-        print_val=""
-        for i in range(0,len(self.val)):
-            s += self.val[i]
-            if self.val[i] == ';' or self.val[i] == ',':
-                s += '\n' + Line.add_indent(eff_indent)
-            elif self.val[i] == '}':
-                eff_indent -= 1
-                s += '\n' + Line.add_indent(eff_indent)
-            elif self.val[i] == '{':
-                eff_indent += 1 
-                s += '\n' + Line.add_indent(eff_indent)
+        next_indent = self.indent
+        s =""
 
-        return eff_indent
+        line = self.val.strip()
+
+        #handle multiline comments "/**/", read till the end
+        if Line.commContinue == 1:
+            if line.endswith('*/\n'):
+                Line.commContinue = 0
+            return next_indent
+
+        if line.startswith('//'):
+            return next_indent
+        elif line.startswith('/*'):
+            if not line.endswith('*/\n'):
+                Line.commContinue = 1
+            return next_indent
+
+        for i in range(0,len(line)):
+            if line[i] == ';':
+                s += line[i] + '\n'
+                if len(line) > i+1:
+                    s += Line.add_indent(next_indent)
+            elif line[i] == '}':
+                next_indent -= 1
+                s += '\n' +  Line.add_indent(next_indent) + line[i] + '\n' + Line.add_indent(next_indent)
+            elif line[i] == '{':
+                next_indent += 1
+                s += line[i] + '\n'
+                if len(line) > i+1:
+                    s += Line.add_indent(next_indent)
+            else:
+                s += line[i]
+#            print "##"+s
+
+        self.val = s
+        return next_indent
 
 
 prevLine = None 
 currLine = None
-currIndent = 0
+next_ind = 0
 
 with open(sys.argv[1]) as infile:
     for line in infile:
-        currLine = Line(line)
+        currLine = Line(line, next_ind)
         
-        currLine.indent = currIndent
+        next_ind = currLine.process()
 
-        line = line.strip()
+   #     line = line.strip()
 
-        #if line.startswith('//',0,2): we don't care this case, just print
-        if line.startswith('}') and not prevLine.val.strip().endswith('{'):
-            currIndent -= 1
-            currLine.indent = currIndent
+   #     #if line.startswith('//',0,2): we don't care this case, just print
+   #     if line.startswith('}') and not prevLine.val.strip().endswith('{'):
+   #         currIndent -= 1
+   #         currLine.indent = currIndent
 
-        if line.endswith('{'):
-            currIndent += 1
+   #     if line.endswith('{'):
+   #         currIndent += 1
             
         currLine.line_print()
                   
